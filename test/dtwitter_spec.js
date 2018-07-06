1a1500ca4d7453636d3b97b28954492507a8777a
emizzle <ericmastro@status.im>
2018-07-06 21:11:00 +1000

    more test updates (WIP)

--- test/dtwitter_spec.js
+++ test/dtwitter_spec.js
@@ -1,4 +1,5 @@
 const DTwitter = require('Embark/contracts/DTwitter');
+let accounts;
 
 config({
   contracts: {
@@ -6,28 +7,44 @@ config({
       // pass args here if needed
     }
   }
+}, (err, theAccounts) => {
+  accounts = theAccounts;
 });
+
+const username = 'testusername';
+const description = 'test description';
+const { createAccount, users, owners, userExists } = DTwitter.methods;
+
+let usernameHash;
 
 describe("DTwitter contract", function() {
 
+  it("should create an owner for current account", async function() {
+
+    // do the create account
+    const result = await createAccount(username, description).send();
+
+    // read from the owners mapping
+    usernameHash = await owners(accounts[0]).call();
+
+    // check the return value from owners mapping matches
+    assert.equal(usernameHash, web3.utils.keccak256(username));
+
+  });
+  
   it("should create a user 'testusername'", async function() {
 
-    const username = 'testusername';
-    const description = 'test description';
-    const { createAccount, users } = DTwitter.methods;
-
-    const result = await createAccount(username, description).send();
-    console.log('result returned from createAccount: ' + JSON.stringify(result));
-
-    const user = await users(username);
-    console.log('user returned from contract: ' + JSON.stringify(user));
+    // get user details from contract
+    const user = await users(usernameHash).call();
 
     assert.equal(user.username, username);
     assert.equal(user.description, description);
+    
   });
 
   it("should know 'testusername' exists", async function() {
-    const exists = await DTwitter.methods.userExists('testusername').send();
+    const exists = await userExists('testusername').call();
+
     assert.equal(exists, true);
   });
 
